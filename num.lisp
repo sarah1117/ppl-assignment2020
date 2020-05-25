72e0dcd27604c53880612f0176c3f7898cecc701
sarah1117 <64269710+sarah1117@users.noreply.github.com>
2020-05-25 11:16:43 +0530

    Update num.lisp

--- num.lisp
+++ num.lisp
@@ -1,8 +1,14 @@
-(defun nth-elt(elt list)
-        "Return element number of ELT in list"
-        (let((loc(length(member elt list))))
-                (unless(zerop loc)
-                        (-(length list)loc)
-                )
-        )
-)       
+(setq my_list '(01 10 20 30 40 50 60 70 80 90 ))
+
+; my function
+(defun nthele (n)
+	(nth n my_list)
+)
+
+(print "List is:" )
+(print my_list)
+(print "Enter element number.")
+(setq n ( read )) 
+; to print the output.
+(print ( nthele n))
+(print "")
