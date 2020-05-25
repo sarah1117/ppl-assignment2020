6e3dba20d742ed547a156bf8d28273afdf2411d2
sarah1117 <64269710+sarah1117@users.noreply.github.com>
2020-05-25 11:18:52 +0530

    Update fact2.lisp

--- fact2.lisp
+++ fact2.lisp
@@ -1,10 +1,9 @@
-(defun factorial(n)
-        (if(=n0)
-                1
-        (*n(factorial(-n1)))
-        )
+(defun facotorial (num)
+	(setq total 1)
+	(do ((i num (- i 1))) ((= i 1))
+		(setq total (* total i))
+	)
+	total
 )
-
-(loop for i from 0 to 16
-        do(format t "~D!=~D~%"i(factoraial i))
-)
+(print "")
+(print ( facotorial (read)))
