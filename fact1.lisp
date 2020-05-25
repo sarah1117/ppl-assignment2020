7820cc3aa1876cbbe9b8af91d35f0bafd2938044
sarah1117 <64269710+sarah1117@users.noreply.github.com>
2020-05-25 11:18:00 +0530

    Update fact1.lisp

--- fact1.lisp
+++ fact1.lisp
@@ -1,5 +1,8 @@
-(defun factorial(num)
-        (cond((zerop num)1)
-                (t(*num(factorial(-num1))))
-        )
+(defun facotorial2 (num &optional (temp num))
+	(if (= num 1)
+		temp
+	( facotorial2 (- num 1) (* (- num 1) temp ))
+	)
 )
+(print ( facotorial2 (read)))
+(print "")
